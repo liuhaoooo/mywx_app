f4766720ca437d26de56badafd6b21d6d2350b39
liuhao <1746581687@qq.com>
2019-11-06 14:21:22 +0800

    '数据库设计'

---
+++ mysql/wechat.sql
@@ -0,0 +1,81 @@
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for collection
+-- ----------------------------
+DROP TABLE IF EXISTS `collection`;
+CREATE TABLE `collection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) NOT NULL,
+  `commodityid` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for dongtai
+-- ----------------------------
+DROP TABLE IF EXISTS `dongtai`;
+CREATE TABLE `dongtai` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) NOT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) NOT NULL,
+  `time` datetime DEFAULT NULL,
+  `dongtai_id` varchar(255) NOT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for dongtai_img
+-- ----------------------------
+DROP TABLE IF EXISTS `dongtai_img`;
+CREATE TABLE `dongtai_img` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dongtai_id` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for gouwuche
+-- ----------------------------
+DROP TABLE IF EXISTS `gouwuche`;
+CREATE TABLE `gouwuche` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) NOT NULL,
+  `commodityid` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `openid` (`openid`),
+  CONSTRAINT `openid` FOREIGN KEY (`openid`) REFERENCES `user` (`openid`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for store
+-- ----------------------------
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE `store` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `fenlei` varchar(255) NOT NULL,
+  `details` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `openid` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `openid` (`openid`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
